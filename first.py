638c8faf6b34d61db29457344d430e20d0a58b93
unknown <ukrantandukar13@gmail.com>
2022-08-11 11:51:28 +0545

    string in python

--- first.py
+++ first.py
@@ -4,8 +4,13 @@
 It represents a multi line comment
 we can write any thing here 
 '''
+from hashlib import new
+from runpy import _ModifiedArgv0
+
+
 a=5 #here, a is a variable which sores/holds integer type value i.e 5
 name="Ukran" #here, name is a variable which stores/holds string type value i.e "Ukran"
 
 # And in python string are always inside single or double quotes:
 # for example :"Ukran" or 'Ukran'
+ file new
