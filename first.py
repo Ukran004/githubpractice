7450734b8b13a684c341f15809653cb292578145
unknown <ukrantandukar13@gmail.com>
2022-08-11 11:41:54 +0545

    string in python

---
+++ first.py
@@ -0,0 +1,11 @@
+# --> It represents a one line comment in python
+
+'''
+It represents a multi line comment
+we can write any thing here 
+'''
+a=5 #here, a is a variable which sores/holds integer type value i.e 5
+name="Ukran" #here, name is a variable which stores/holds string type value i.e "Ukran"
+
+# And in python string are always inside single or double quotes:
+# for example :"Ukran" or 'Ukran'
